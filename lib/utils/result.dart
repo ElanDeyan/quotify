9fbc9ff4880a21e07b50c98e7ccbb58c20a67a0a
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-12-01 23:15:20 -0300

    refactor: renames Error to failure, adds annotation to as* getters and adds stackTrace to failure

--- lib/utils/result.dart
+++ lib/utils/result.dart
@@ -1,8 +1,13 @@
 /// Copied from https://github.com/flutter/samples/blob/main/compass_app/app/lib/utils/result.dart
 /// with subtle differences:
-/// - Type parameter in asError getter.
-/// - Result factories translated to const factories.
+/// - Renames [Error] to [Failure].
+/// - Type parameter in [Result.asFailure] getter.
+/// - [Result] factories translated to const factories.
+/// - [StackTrace] class inside the [Failure].
+/// - Also some comments.
 library;
+
+import 'package:meta/meta.dart';
 
 // Copyright 2024 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
@@ -28,13 +33,16 @@ sealed class Result<T> {
   const factory Result.ok(T value) = Ok;
 
   /// Create an instance of Result containing an error
-  const factory Result.error(Exception error) = Error;
+  const factory Result.failure(Exception failure, StackTrace stackTrace) =
+      Failure;
 
-  /// Convenience method to cast to Ok
+  /// Convenience method to cast to [Ok]
+  @visibleForTesting
   Ok<T> get asOk => this as Ok<T>;
 
-  /// Convenience method to cast to Error
-  Error<T> get asError => this as Error<T>;
+  /// Convenience method to cast to [Failure]
+  @visibleForTesting
+  Failure<T> get asFailure => this as Failure<T>;
 }
 
 /// Subclass of Result for values
@@ -49,12 +57,14 @@ final class Ok<T> extends Result<T> {
 }
 
 /// Subclass of Result for errors
-final class Error<T> extends Result<T> {
-  const Error(this.error);
+final class Failure<T> extends Result<T> {
+  const Failure(this.failure, this.stackTrace);
 
   /// Returned error in result
-  final Exception error;
+  final Exception failure;
+
+  final StackTrace stackTrace;
 
   @override
-  String toString() => 'Result<$T>.error($error)';
+  String toString() => 'Result<$T>.error(\n\t$failure,\n\t$stackTrace,\n)';
 }
