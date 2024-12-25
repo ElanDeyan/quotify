1a2583e96ef931ce645e5623a560fde9a2bf392b
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-12-25 16:33:28 -0300

    chore: exports Maybe and bumps to 1.3.1

--- packages/quotify_utils/lib/quotify_utils.dart
+++ packages/quotify_utils/lib/quotify_utils.dart
@@ -4,6 +4,7 @@ library;
 
 export 'src/future_result.dart';
 export 'src/id.dart';
+export 'src/maybe.dart';
 export 'src/natural.dart';
 export 'src/non_blank_string.dart';
 export 'src/result.dart';
