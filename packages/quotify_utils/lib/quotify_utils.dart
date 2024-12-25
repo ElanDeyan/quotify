f40a53af4a5fcef3e4594b2d872ce85dcd46f29f
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-12-25 16:23:05 -0300

    chore: update quotify_utils to include new exports and bump version to 1.2.0

--- packages/quotify_utils/lib/quotify_utils.dart
+++ packages/quotify_utils/lib/quotify_utils.dart
@@ -1,6 +1,8 @@
-/// Utilities for help in the development of the 
+/// Utilities for help in the development of the
 /// quotify app.
 library;
 
 export 'src/future_result.dart';
+export 'src/natural.dart';
+export 'src/non_blank_string.dart';
 export 'src/result.dart';
