703d62557b23df8fe5458a22bb015e398ea25a3b
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2025-01-05 15:46:16 -0300

    fix!: exports and bumps version to 1.0.0

---
+++ packages/services/drift_database_service/lib/drift_database_service.dart
@@ -0,0 +1,7 @@
+export 'src/database/app_database.dart';
+export 'src/database/connection/native_connection.dart';
+export 'src/database/connection/setup_sql_cipher.dart';
+export 'src/database/restore_config.dart';
+export 'src/exceptions/database_errors.dart';
+export 'src/extensions/tag_entry_extension.dart';
+export 'src/extensions/tag_model_extension.dart';
