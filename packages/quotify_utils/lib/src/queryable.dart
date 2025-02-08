2c61c3052d62daf4cfed9d9d6c5b167fd4e274f1
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2025-02-08 12:09:01 -0300

    feat: adds Queryable interface

---
+++ packages/quotify_utils/lib/src/queryable.dart
@@ -0,0 +1,21 @@
+/// Interface for making classes queryable.
+abstract interface class Queryable {
+  /// Converts this to a string that will be used with [hasMatchWith] method.
+  String get asQueryableString;
+
+  /// Matches [asQueryableString] with [string] using [RegExp].
+  bool hasMatchWith(
+    String string, {
+    bool caseSensitive = false,
+    bool multiline = true,
+    bool dotAll = false,
+    bool unicode = false,
+  }) =>
+      RegExp(
+        string,
+        caseSensitive: caseSensitive,
+        dotAll: dotAll,
+        multiLine: multiline,
+        unicode: unicode,
+      ).hasMatch(asQueryableString);
+}
