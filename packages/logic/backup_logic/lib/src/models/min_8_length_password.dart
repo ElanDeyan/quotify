35b77344df94899e588827bd08aff6c172c2b72f
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2025-02-23 16:20:18 -0300

    docs: adds docs

--- packages/logic/backup_logic/lib/src/models/min_8_length_password.dart
+++ packages/logic/backup_logic/lib/src/models/min_8_length_password.dart
@@ -1,5 +1,8 @@
+/// Extension type for passwords composed by [String] with [String.length] >= 8.
 extension type const Min8LengthPassword._(String _self)
     implements String, Object {
+  /// Extension type for passwords composed by [String] with [String.length] >= 
+  /// 8.
   const Min8LengthPassword(this._self)
-      : assert(_self.length >= 8, 'Should have at least 8 character length');
+    : assert(_self.length >= 8, 'Should have at least 8 character length');
 }
