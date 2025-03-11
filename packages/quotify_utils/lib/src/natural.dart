e8f4d72578a3473da4e5cc8fb3a4eea9e283f9d4
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2025-03-10 22:46:18 -0300

    feat: adds static method for Natural and adds lint and updates included import at analysis_options

--- packages/quotify_utils/lib/src/natural.dart
+++ packages/quotify_utils/lib/src/natural.dart
@@ -6,6 +6,9 @@ extension type const Natural._(int _self) implements int, Object {
   /// An [int] extension for >= 0 integers.
   const Natural(this._self)
       : assert(_self >= 0, 'Integer should be higher than zero');
+
+  /// Well... zero.
+  static const zero = Natural._(0);
 }
 
 /// Extension on [int] to convert an integer to a [Natural] number.
